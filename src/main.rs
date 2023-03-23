bea5faa1f35d56c7cbaf5d47c9909e2254e103fa
rubydusa <rubydusa@protonmail.com>
2023-03-23 19:22:56 +0200

    forget about caching for now

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use ropey::Rope;
-use tower_lsp::jsonrpc::Result;
+use tower_lsp::jsonrpc;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
 use codespan_reporting::diagnostic::{Severity, LabelStyle};
 use circom_structure::error_definition::Report;
 
-use std::path::PathBuf;
-use std::collections::HashMap;
-use std::cell::RefCell;
 use std::sync::Mutex;
+use std::cell::RefCell;
+use std::collections::HashMap;
 
 struct TextDocumentItem {
     uri: Url,
@@ -20,7 +19,7 @@ struct TextDocumentItem {
 #[derive(Debug)]
 struct Backend {
     client: Client,
-    document_map: Mutex<RefCell<HashMap<Url, Rope>>>,
+    document_map: Mutex<RefCell<HashMap<Url, Rope>>>
 }
 
 impl Backend {
@@ -31,69 +30,44 @@ impl Backend {
         }
     }
 
-    async fn debug(&self, message: &str) {
-        self.client
-            .log_message(MessageType::ERROR, message)
-            .await;
-    }
-
-    async fn on_change(&self, params: TextDocumentItem, publish_diagnostics: bool) {
-        self.debug("on_change").await;
+    async fn on_change(&self, params: TextDocumentItem) {
         let path = url_to_string(&params.uri);
         let rope = Rope::from_str(&params.text);
 
-        if publish_diagnostics {
-            let diagnostics: Vec<_> = {
-                self.debug("computing cache").await;
-                let cache = {
-                    let document_map = self.document_map.lock().unwrap();
-                    let document_map = document_map.borrow();
-                    document_map_to_cache(&document_map)
-                };
-                // let cache = HashMap::new();
-
-                self.debug("computing reports").await;
-                let reports = match circom_parser::run_parser_cached(
-                    path, 
-                    "2.1.4", 
-                    vec![], 
-                    cache, 
-                    true
-                ) {
-                    Ok((mut archive, mut reports)) => {
-                        let mut type_reports = match circom_type_checker::check_types::check_types(&mut archive) {
-                            Ok(type_reports) => type_reports,
-                            Err(type_reports) => type_reports
-                        };
-                        reports.append(&mut type_reports);
-                        reports
-                    },
-                    Err((_, reports)) => reports
-                };
-
-                self.debug("convert reports to diagnostics").await;
-                reports
-                    .into_iter()
-                    .map(|x| Self::report_to_diagnostic(&rope, x))
-                    .collect()
+        let diagnostics: Vec<_> = {
+            let reports = match circom_parser::run_parser(
+                path, 
+                "2.1.4", 
+                vec![], 
+            ) {
+                Ok((mut archive, mut reports)) => {
+                    let mut type_reports = match circom_type_checker::check_types::check_types(&mut archive) {
+                        Ok(type_reports) => type_reports,
+                        Err(type_reports) => type_reports
+                    };
+                    reports.append(&mut type_reports);
+                    reports
+                },
+                Err((_, reports)) => reports
             };
 
-            self.debug(&format!("{:?}", diagnostics)).await;
+            reports
+                .into_iter()
+                .filter_map(|x| Self::report_to_diagnostic(&rope, x).ok())
+                .collect()
+        };
 
-            self.client
-                .publish_diagnostics(params.uri.clone(), diagnostics, params.version)
-                .await;
+        self.client
+            .publish_diagnostics(params.uri.clone(), diagnostics, params.version)
+            .await;
 
-            self.debug("doesn't reach as far as I understand").await;
-        }
-
-        self.debug("document insertion").await;
         let document_map = self.document_map.lock().unwrap();
         document_map.borrow_mut().insert(params.uri, rope);
     }
 
     // convert circom report to a lsp diagnostic
-    fn report_to_diagnostic(rope: &Rope, report: Report) -> Diagnostic {
+    // can only fail on label ranges, that's
+    fn report_to_diagnostic(rope: &Rope, report: Report) -> ropey::Result<Diagnostic> {
         let diagnostic = report.to_diagnostic();
 
         let label = diagnostic.labels.into_iter().reduce(|cur, a| {
@@ -110,8 +84,8 @@ impl Backend {
 
         let range = match label {
             Some(label) => Range {
-                start: char_to_position(rope, label.range.start),
-                end: char_to_position(rope, label.range.end)
+                start: char_to_position(rope, label.range.start)?,
+                end: char_to_position(rope, label.range.end)?
             },
             None => Range {
                 start: Position { line: 0, character: 0 },
@@ -128,7 +102,7 @@ impl Backend {
 
         let message = diagnostic.message;
 
-        Diagnostic {
+        Ok(Diagnostic {
             range,
             severity: Some(severity),
             code: None,
@@ -138,13 +112,13 @@ impl Backend {
             related_information: None,
             tags: None,
             data: None
-        }
+        })
     }
 }
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+    async fn initialize(&self, _: InitializeParams) -> jsonrpc::Result<InitializeResult> {
         Ok(InitializeResult {
             capabilities: ServerCapabilities {
                 text_document_sync: Some(TextDocumentSyncCapability::Kind(
@@ -162,7 +136,7 @@ impl LanguageServer for Backend {
             .await;
     }
 
-    async fn shutdown(&self) -> Result<()> {
+    async fn shutdown(&self) -> jsonrpc::Result<()> {
         Ok(())
     }
 
@@ -176,25 +150,11 @@ impl LanguageServer for Backend {
                 uri: params.text_document.uri,
                 text: params.text_document.text,
                 version: Some(params.text_document.version)
-            },
-            false
-        ).await;
-    }
-
-    async fn did_change(&self, params: DidChangeTextDocumentParams) {
-        self.debug("did change").await;
-        self.on_change(
-            TextDocumentItem {
-                uri: params.text_document.uri,
-                text: params.content_changes[0].text.clone(),
-                version: Some(params.text_document.version)
-            },
-            false
+            }
         ).await;
     }
 
     async fn did_save(&self, params: DidSaveTextDocumentParams) {
-        self.debug("did save").await;
         let result = {
             let document_map = self.document_map.lock().unwrap();
             let document_map = document_map.borrow();
@@ -208,35 +168,26 @@ impl LanguageServer for Backend {
                         uri: params.text_document.uri,
                         text: document,
                         version: None
-                    },
-                    true
+                    }
                 ).await;
             },
-            None => self.debug("could not find document").await
+            None => ()
         };
     }
 }
 
-fn char_to_position(rope: &Rope, idx: usize) -> Position {
-    let line = rope.char_to_line(idx);
+fn char_to_position(rope: &Rope, idx: usize) -> ropey::Result<Position> {
+    let line = rope.try_char_to_line(idx)?;
     let line_start = rope.line_to_char(line);
     let character = idx - line_start;
 
     let line = u32::try_from(line).unwrap();
     let character = u32::try_from(character).unwrap();
 
-    Position {
+    Ok(Position {
         line,
         character
-    }
-}
-
-fn document_map_to_cache(document_map: &HashMap<Url, Rope>) -> HashMap<PathBuf, String> {
-    document_map.iter().map(|(file_uri, document)| {
-        let (path, content) = (PathBuf::from(url_to_string(&file_uri)), document.to_string());
-
-        (path, content)
-    }).collect()
+    })
 }
 
 fn url_to_string(url: &Url) -> String {
